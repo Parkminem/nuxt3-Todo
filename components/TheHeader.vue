464e3513ddf6bb3857d15604adafc7d5d5b950d5
Minhyuk Park <parkminem@gmail.com>
2023-03-31 13:28:26 +0900

    setting nuxt3 && typescript

---
+++ components/TheHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <h1>Header</h1>
+    </header>
+</template>
+
+<script setup></script>
+
+<style lang="scss">
+header {
+    width: 100%;
+    height: 100%;
+    border: 1px solid black;
+}
+</style>
